955a437172f61cd415803e2efe21b4c7bd4b9682
TurnerMooreDavis <turnermdavis@gmail.com>
2015-12-24 19:24:12 -0500

    change env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,8 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => ENV['EMAIL'],
-    :password             => ENV['EMAIL_PASSWORD'],
+    :user_name            => ENV["EMAIL"],
+    :password             => ENV["EMAIL_PASSWORD"],
     :authentication       => :login,
     :enable_starttls_auto => true
   }

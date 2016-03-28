09d12ad061e4ab36986b654b9f842fe0639e7872
TurnerMooreDavis <turnermdavis@gmail.com>
2016-03-27 23:46:37 -0400

    more sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,6 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.contycontent.com"
-SitemapGenerator::Sitemap.public_path = 'tmp/sitemaps/'
-
+SitemapGenerator::Sitemap.sitemaps_path = 'shared/'
 SitemapGenerator::Sitemap.create do
   add '/contact_me'
   add '/portfolio'
